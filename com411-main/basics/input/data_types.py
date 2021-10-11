0b238743efb86534d0af01868c138deb94db8a21
Arthur Marques <5marqa82@solent.ac.uk>
2021-10-11 16:16:44 +0100

    Still adding code to demonstrate the use of different data types.

---
+++ com411-main/basics/input/data_types.py
@@ -0,0 +1,22 @@
+# Ask user to enter their name
+print("What is your name human?")
+name = input()
+print(f"It is nice to meet you human {name}")
+
+# Ask user to enter their age
+print("How old are you (in years)?")
+user_age = int(input())
+print(f"Your age is {user_age}")
+
+# Ask user to enter their height
+print("How tall are you (in meters)?")
+user_height = float(input())
+print(f"Your height is {user_height}")
+
+# Ask user to enter their weight
+print("How much do you weight (in kilograms)?")
+user_weight = int(input())
+print(f"Your weight is {user_weight}")
+
+# Calculate the user's BMI
+
